07d7f366054efad4dc9164c427bfe4b0906ad1f9
rizvisyed01 <syed.rizvi@student.howest.be>
2017-08-29 14:05:44 +0200

    Entities added

---
+++ FinalHerhalingApi/src/FinalHerhalingApi/Entities/Boete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalHerhalingApi.Entities
+{
+    public partial class Boete
+    {
+        public int Studnr { get; set; }
+        public int? Boet { get; set; }
+
+        public virtual Studenten StudnrNavigation { get; set; }
+    }
+}
